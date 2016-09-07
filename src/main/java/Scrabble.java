1c6b2e0cc762ebff57650f5680a2770ea8087501
Caleb Paul and Caleb Stevenson <caleb@>
2016-09-07 13:53:46 -0700

    add branching for 1st spec letter group with passing test

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -3,6 +3,12 @@
 public class Scrabble {
   public Integer calculateScore(String word) {
     Integer result = 0;
+    for (Integer i = 0; i < word.length(); i++) {
+      char at = word.charAt(i);
+      if ((at == 'a') || (at == 'e') || (at == 'i') || (at == 'o') || (at == 'u') || (at == 'l') || (at == 'n') || (at == 'r') || (at == 's') || (at == 't')) {
+        result += 1;
+      }
+    }
     return result;
   }
 }
